98d53f483b009b6ce9b09f85ba1ee0bc3d9d46c5
Nisaruj <nisaruj@hotmail.com>
2018-05-18 17:00:36 +0700

    Add custom test and Optimize judging code

--- controllers/problem_controller.js
+++ controllers/problem_controller.js
@@ -31,55 +31,43 @@ exports.get_problem = function(req, res) {
 };
 
 exports.post_submission = function(req, res, next) {
-    var get_result = function(tokens, sourcecode) {
-        let options = [];
-        for(var i=0;i<tokens.length;i++) {
-            options.push({
-                method: 'GET',
-                uri: 'https://api.judge0.com/submissions/' + tokens[i].token + '?base64_encoded=false',
-                json: true
+    var get_result = function(data, sourcecode) {
+        //console.log(data);
+        var result = '', score = 0, time_avg = 0, mem_avg = 0;
+        for (var i=0;i<data.length;i++) {
+            time_avg += parseFloat(data[i].time);
+            mem_avg += data[i].memory;
+            if (data[i].status.id === 3) {
+                result += 'P';
+                score++;
+            }
+            else if (data[i].status.id === 4 || data[i].status.id === 13) result += '-';
+            else if (data[i].status.id === 5) result += 'T';
+            else if (data[i].status.id === 6) {
+                result = 'Compilation Error';
+                break;
+            } else result += 'X';
+        }
+        var new_submission = new Submission({
+            pid: req.params.pid,
+            lang: lang[parseInt(req.body.lang)-1].name,
+            username: req.user ? req.user.username : 'Guest',
+            sourcecode: sourcecode,
+            submit_time: new Date(),
+            result: {str: result, time: time_avg/data.length, memory: mem_avg/data.length}
+        });
+        new_submission.save(function(err) {
+            if (err) console.log(err);
+        });
+        if (score === data.length) {
+            Problem.findOneAndUpdate({pid : req.params.pid}, {$inc : {solved : 1}}, function(err){
+                if(err) console.log(err);
             });
         }
-        const promises = options.map(opt => request(opt));
-        Promise.all(promises).then((data) => {
-            //console.log(data);
-            var result = '', score = 0, time_avg = 0, mem_avg = 0;
-            for (var i=0;i<data.length;i++) {
-                time_avg += parseFloat(data[i].time);
-                mem_avg += data[i].memory;
-                if (data[i].status.id === 3) {
-                    result += 'P';
-                    score++;
-                }
-                else if (data[i].status.id === 4 || data[i].status.id === 13) result += '-';
-                else if (data[i].status.id === 5) result += 'T';
-                else if (data[i].status.id === 6) {
-                    result = 'Compilation Error';
-                    break;
-                } else result += 'X';
-            }
-            var new_submission = new Submission({
-                pid: req.params.pid,
-                lang: lang[parseInt(req.body.lang)-1].name,
-                username: req.user ? req.user.username : 'Guest',
-                sourcecode: sourcecode,
-                submit_time: new Date(),
-                result: {str: result, time: time_avg/data.length, memory: mem_avg/data.length}
-                /* TODO : ADD PROBLEM ID */
-            });
-            new_submission.save(function(err) {
-                if (err) console.log(err);
-            });
-            if (score === data.length) {
-                Problem.findOneAndUpdate({pid : req.params.pid}, {$inc : {solved : 1}}, function(err){
-                    if(err) console.log(err);
-                });
-            }
-            Problem.findOne({avail: true, pid: req.params.pid}, function (err, prob_res) {
-                if (err) return console.log(err);
-                res.cookie('submitLang' , req.body.lang)
-                .render('problem', {user: req.user, content: prob_res, result: result, accepted: score === data.length, submitLang: req.cookies.submitLang, langlist: lang});
-            });
+        Problem.findOne({avail: true, pid: req.params.pid}, function (err, prob_res) {
+            if (err) return console.log(err);
+            res.cookie('submitLang' , req.body.lang)
+            .render('problem', {user: req.user, content: prob_res, result: result, accepted: score === data.length, submitLang: req.cookies.submitLang, langlist: lang});
         });
     }
     //console.log(req.file);
@@ -115,55 +103,44 @@ exports.post_submission = function(req, res, next) {
 };
 
 exports.post_submission_live_editor = function(req, res, next) {
-    var get_result = function(tokens, sourcecode) {
-        let options = [];
-        for(var i=0;i<tokens.length;i++) {
-            options.push({
-                method: 'GET',
-                uri: 'https://api.judge0.com/submissions/' + tokens[i].token + '?base64_encoded=false',
-                json: true
+    var get_result = function(data, sourcecode) {
+        //console.log(data);
+        var result = '', score = 0, time_avg = 0, mem_avg = 0;
+        for (var i=0;i<data.length;i++) {
+            time_avg += parseFloat(data[i].time);
+            mem_avg += data[i].memory;
+            if (data[i].status.id === 3) {
+                result += 'P';
+                score++;
+            }
+            else if (data[i].status.id === 4 || data[i].status.id === 13) result += '-';
+            else if (data[i].status.id === 5) result += 'T';
+            else if (data[i].status.id === 6) {
+                result = 'Compilation Error';
+                break;
+            } else result += 'X';
+        }
+        var new_submission = new Submission({
+            pid: req.params.pid,
+            lang: lang[parseInt(req.body.lang)-1].name,
+            username: req.user ? req.user.username : 'Guest',
+            sourcecode: sourcecode,
+            submit_time: new Date(),
+            result: {str: result, time: time_avg/data.length, memory: mem_avg/data.length}
+            /* TODO : ADD PROBLEM ID */
+        });
+        new_submission.save(function(err) {
+            if (err) console.log(err);
+        });
+        if (score === data.length) {
+            Problem.findOneAndUpdate({pid : req.params.pid}, {$inc : {solved : 1}}, function(err){
+                if(err) console.log(err);
             });
         }
-        const promises = options.map(opt => request(opt));
-        Promise.all(promises).then((data) => {
-            //console.log(data);
-            var result = '', score = 0, time_avg = 0, mem_avg = 0;
-            for (var i=0;i<data.length;i++) {
-                time_avg += parseFloat(data[i].time);
-                mem_avg += data[i].memory;
-                if (data[i].status.id === 3) {
-                    result += 'P';
-                    score++;
-                }
-                else if (data[i].status.id === 4 || data[i].status.id === 13) result += '-';
-                else if (data[i].status.id === 5) result += 'T';
-                else if (data[i].status.id === 6) {
-                    result = 'Compilation Error';
-                    break;
-                } else result += 'X';
-            }
-            var new_submission = new Submission({
-                pid: req.params.pid,
-                lang: lang[parseInt(req.body.lang)-1].name,
-                username: req.user ? req.user.username : 'Guest',
-                sourcecode: sourcecode,
-                submit_time: new Date(),
-                result: {str: result, time: time_avg/data.length, memory: mem_avg/data.length}
-                /* TODO : ADD PROBLEM ID */
-            });
-            new_submission.save(function(err) {
-                if (err) console.log(err);
-            });
-            if (score === data.length) {
-                Problem.findOneAndUpdate({pid : req.params.pid}, {$inc : {solved : 1}}, function(err){
-                    if(err) console.log(err);
-                });
-            }
-            Problem.findOne({avail: true, pid: req.params.pid}, function (err, prob_res) {
-                if (err) return console.log(err);
-                res.cookie('submitLang' , req.body.lang)
-                .render('problem', {user: req.user, content: prob_res, result: result, accepted: score === data.length, submitLang: req.cookies.submitLang, langlist: lang});
-            });
+        Problem.findOne({avail: true, pid: req.params.pid}, function (err, prob_res) {
+            if (err) return console.log(err);
+            res.cookie('submitLang' , req.body.lang)
+            .render('problem', {user: req.user, content: prob_res, result: result, accepted: score === data.length, submitLang: req.cookies.submitLang, langlist: lang});
         });
     }
     //console.log(req.file);
@@ -191,6 +168,29 @@ exports.post_submission_live_editor = function(req, res, next) {
             get_result(data, req.body.sourcecode);
             //console.log(data);
         });
-        //console.log(test_res);
+    });
+};
+
+exports.get_custom_test = function(req, res) {
+    res.render('custom_test', {user: req.user, submitLang: req.cookies.submitLang, langlist: lang, result: null, request: null});
+};
+
+exports.post_custom_test_live = function(req, res) {
+    console.log(req.body.sourcecode);
+    var options = {
+        method: 'POST',
+        uri: 'https://api.judge0.com/submissions/?base64_encoded=false&wait=true',
+        body: {
+            "source_code": req.body.sourcecode,
+            "language_id": parseInt(req.body.lang),
+            "stdin": req.body.input
+        },
+        json: true
+    };
+    request(options, function(err, result, body) {
+        //console.log(body);
+        console.log(body);
+        res.cookie('submitLang' , req.body.lang);
+        res.render('custom_test', {user: req.user, submitLang: req.cookies.submitLang, langlist: lang, result: body, request: {stdin: req.body.input, sourcecode: req.body.sourcecode}});
     });
 };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var auth_controller = require('../controllers/auth_controller');
+var problem_controller = require('../controllers/problem_controller');
 
 router.get('/', function(req, res) {
     res.render('index', {user: req.user});
@@ -22,5 +23,13 @@ router.post('/register', auth_controller.post_register);
 router.get('/auth/facebook', auth_controller.fb_login);
 
 router.get('/auth/facebook/callback', auth_controller.fb_login_cb);
+
+router.get('/custom', problem_controller.get_custom_test);
+
+router.get('/custom/live', function(req, res) {
+    res.redirect('/custom');
+});
+
+router.post('/custom/live', problem_controller.post_custom_test_live);
 
 module.exports = router;
