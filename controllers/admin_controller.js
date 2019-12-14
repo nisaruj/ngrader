c0adde180add8c0318558278f2a056fc4d759fc8
Nisaruj <nisaruj@hotmail.com>
2019-12-14 12:50:19 +0700

    Fix update problem avail bug

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -102,14 +102,17 @@ exports.get_submission = function(req, res) {
 };
 
 exports.update_avail = function(req, res) {
-    //console.log(req.body.avail);
+    // console.log(req.body.avail);
     const is_avail = new Set(req.body.avail.map(function(num) { return parseInt(num,10); }));
     Problem.find({},function(err, problem_res) {
         for (var i=0;i<problem_res.length;i++) {
-            problem_res[i].avail = is_avail.has(i);
-            Problem.update({_id: problem_res[i]._id}, {avail: problem_res[i].avail}, function(err, up_res) {
-                if (err) console.log(err);
-            });
+            const avail = is_avail.has(problem_res[i].pid);
+            if (avail != problem_res[i].avail) {
+                Problem.updateOne({_id: problem_res[i]._id}, {avail}, function(err, up_res) {
+                    if (err) console.log(err);
+                    // console.log(up_res)
+                });
+            }
         }
         res.redirect('/admin');
     });
